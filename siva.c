0567dd3e7f507bf45694e96a8c3139d11cfd7388
Pradeep75985 <92733960+Pradeep75985@users.noreply.github.com>
2021-10-18 20:15:49 +0530

    swapping of two numbers
    printf function is used to print a function

---
+++ siva.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a=5, b=6;
+printf(\n The number before swap is: a=%d, b=%d",a,b);
+a=a+b;
+b=a-b;
+printf(\n The number after swap is: a=%d, b=%d",a,b);
+return 0;
+}
